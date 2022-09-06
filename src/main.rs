d86ff00a82a478398e65be6905426dff8fd9a59d
Viktor Holta <viktor.holta@outlook.com>
2022-09-06 17:55:28 +0200

    Initialize and clean up terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use crossterm::{terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}, execute};
+use tui::{backend::CrosstermBackend, Terminal};
+
+fn main() -> io::Result<()> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    enable_raw_mode()?;
+    execute!(terminal.backend_mut(), EnterAlternateScreen)?;
+
+    // TODO: add render and logic code
+
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+
+    Ok(())
 }
