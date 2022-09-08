5e629cc4baf8542baf0e4dfe01a961e45baf2b23
Viktor Holta <viktor.holta@outlook.com>
2022-09-08 14:38:47 +0200

    Represent each line as a separate string & add ability to write characters

--- src/app.rs
+++ src/app.rs
@@ -1,28 +1,52 @@
 use std::{io, time::Duration};
 
 use crossterm::event::{self, Event, KeyModifiers, KeyCode};
-use tui::{Terminal, backend::Backend, Frame, widgets::Paragraph};
+use tui::{Terminal, backend::Backend, Frame, widgets::Paragraph, text::{Text, Spans}};
 
 use self::unicode::UnicodeString;
 
 pub mod unicode;
 
-fn render<B: Backend>(frame: &mut Frame<B>, text: &UnicodeString) {
-    let paragraph = Paragraph::new(text.as_str());
+fn render<B: Backend>(frame: &mut Frame<B>, lines: &Vec<UnicodeString>) {
+    let mut lines_spans = Vec::new();
+    for line in lines {
+        lines_spans.push(Spans::from(line.as_str()));
+    }
+    let text = Text::from(lines_spans);
+    
+    let paragraph = Paragraph::new(text);
     frame.render_widget(paragraph, frame.size());
 }
 
 pub fn run<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
     let poll_duration = Duration::from_millis(500);
-    let text = UnicodeString::from("This is text.");
+
+    let mut lines = vec![UnicodeString::new()];
+    let mut target_line: usize = 0;
+    let mut target_char: usize = 0;
 
     loop {
-        terminal.draw(|frame| render(frame, &text))?;
+        terminal.draw(|frame| render(frame, &lines))?;
 
         if event::poll(poll_duration)? {
             if let Event::Key(key) = event::read()? {
                 if key.modifiers == KeyModifiers::CONTROL && key.code == KeyCode::Char('c') {
                     return Ok(());
+                }
+
+                let curr_line = lines.get_mut(target_line)
+                    .expect("should never index a line out-of-bounds");
+
+                if key.code == KeyCode::Enter {
+                    // TODO: implement UnicodeString::drain first
+                } else if key.code == KeyCode::Backspace {
+                    // TODO: implement UnicodeString::push_str first
+                } else if key.code == KeyCode::Delete {
+                    // TODO: implement UnicodeString::push_str first
+                } else if let KeyCode::Char(ch) = key.code {
+                    target_char = target_char.clamp(0, curr_line.length());
+                    curr_line.insert(target_char, ch);
+                    target_char += 1;
                 }
             }
         }
