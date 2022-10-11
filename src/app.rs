d9fa03282fc727b2b7b095cd55cab5406c5ec299
Viktor Holta <viktor.holta@outlook.com>
2022-10-11 08:59:21 +0200

    Add rendering for editor & file path prompt

--- src/app.rs
+++ src/app.rs
@@ -1,23 +1,63 @@
 use std::{io, time::Duration};
 
-use crossterm::event::{self, Event, KeyModifiers, KeyCode};
-use tui::{Terminal, backend::Backend, Frame, widgets::{Paragraph, Block, Borders}, layout::Rect};
+use crossterm::event::{self, Event, KeyModifiers, KeyCode, KeyEvent};
+use tui::{Terminal, backend::Backend, Frame};
 
-fn render<B: Backend>(frame: &mut Frame<B>) {
-    
+use self::{open_file::OpenFile, file_path::FilePath};
+
+mod open_file;
+mod file_path;
+mod unicode;
+
+// TODO: create AppState struct to hold all data
+
+fn render<B: Backend>(frame: &mut Frame<B>, open_file: &mut OpenFile, file_path: &mut FilePath, is_saving: bool) {
+    open_file.render(frame);
+    if is_saving {
+        file_path.render(frame);
+    }
+}
+
+fn edit_events(key: &KeyEvent, open_file: &mut OpenFile, file_path: &mut FilePath, is_saving: &mut bool, is_qutting: &mut bool) {
+    if key.modifiers == KeyModifiers::CONTROL && key.code == KeyCode::Char('c') {
+        *is_qutting = true;
+    } else if key.modifiers == KeyModifiers::CONTROL && key.code == KeyCode::Char('s') {
+        *is_saving = true;
+        *file_path = open_file.path.clone();
+    }
+}
+
+fn save_events(key: &KeyEvent, open_file: &mut OpenFile, file_path: &mut FilePath, is_saving: &mut bool, is_qutting: &mut bool) {
+    if key.modifiers == KeyModifiers::CONTROL && key.code == KeyCode::Char('c') {
+        *is_saving = false;
+    } else if key.code == KeyCode::Enter {
+        open_file.path = file_path.clone();
+        // TODO: save open file lines to path (create directories if necessary)
+    }
 }
 
 pub fn run<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
     let poll_duration = Duration::from_millis(500);
 
+    let mut is_quitting = false;
+    let mut is_saving = false;
+    let mut open_file = OpenFile::new();
+    let mut file_path = FilePath::new();
+
     loop {
-        terminal.draw(|frame| render(frame))?;
+        terminal.draw(|frame| render(frame, &mut open_file, &mut file_path, is_saving))?;
+
+        if is_quitting {
+            return Ok(()); // TODO: warn about unsaved changes
+        }
 
         if event::poll(poll_duration)? {
             if let Event::Key(key) = event::read()? {
-                if key.modifiers == KeyModifiers::CONTROL && key.code == KeyCode::Char('c') {
-                    return Ok(());
-                } 
+                if is_saving {
+                    save_events(&key, &mut open_file, &mut file_path, &mut is_saving, &mut is_quitting);
+                } else {
+                    edit_events(&key, &mut open_file, &mut file_path, &mut is_saving, &mut is_quitting);
+                }
             }
         }
     }
---
+++ src/app/unicode.rs
@@ -0,0 +1,191 @@
+use std::{string::Drain, ops::{Index, RangeFrom}};
+
+#[derive(Clone)]
+pub struct UnicodeString {
+    inner_string: String,
+    length: usize,
+    inner_indices: Vec<usize>,
+}
+
+impl UnicodeString {
+    pub fn new() -> Self {
+        return Self {
+            inner_string: String::new(),
+            length: 0,
+            inner_indices: Vec::new(),
+        };
+    }
+
+    pub fn from(src: &str) -> Self {
+        let mut new = Self {
+            inner_string: String::from(src),
+            length: 0,
+            inner_indices: Vec::new(),
+        };
+
+        for (idx, _) in new.inner_string.char_indices() {
+            new.length += 1;
+            new.inner_indices.push(idx);
+        }
+
+        return new;
+    }
+
+    pub fn length(&self) -> usize {
+        return self.length;
+    }
+
+    pub fn push(&mut self, ch: char) {
+        self.inner_indices.push(self.inner_string.len());
+        self.inner_string.push(ch);
+        self.length += 1;
+    }
+
+    pub fn push_str(&mut self, s: &str) {
+        for ch in s.chars() {
+            self.push(ch);
+        }
+    }
+
+    pub fn insert(&mut self, idx: usize, ch: char) {
+        assert!(idx <= self.length);
+
+        if idx == self.length {
+            self.push(ch);
+        } else {
+            let inner_idx = self.inner_indices[idx];
+            self.inner_string.insert(inner_idx, ch);
+
+            self.inner_indices.insert(idx, inner_idx);
+            for inner_idx in &mut self.inner_indices[idx+1..] {
+                *inner_idx += ch.len_utf8();
+            }
+
+            self.length += 1;
+        }
+    }
+
+    pub fn remove(&mut self, idx: usize) {
+        assert!(idx < self.length);
+
+        let inner_idx = self.inner_indices.remove(idx);
+        let ch = self.inner_string.remove(inner_idx);
+
+        for inner_idx in &mut self.inner_indices[idx..] {
+            *inner_idx -= ch.len_utf8();
+        }
+
+        self.length -= 1;
+    }
+
+    pub fn drain(&mut self, start: usize, end: usize) -> Drain {
+        assert!(start <= end);
+        assert!(start <= self.length);
+        assert!(end <= self.length);
+        
+        if start == end {
+            return self.inner_string.drain(0..0);
+        } else {
+            let inner_start = self.inner_indices[start];
+            let inner_end = if end == self.length {
+                self.inner_string.len()
+            } else {
+                self.inner_indices[end]
+            };
+
+            let removed_bytes = inner_end - inner_start;
+            self.inner_indices.drain(start..end);
+            for inner_idx in &mut self.inner_indices[start..] {
+                *inner_idx -= removed_bytes;
+            }
+
+            self.length -= end - start;
+            return self.inner_string.drain(inner_start..inner_end);
+        }
+    }
+
+    pub fn as_str(&self) -> &str {
+        return self.inner_string.as_str();
+    }
+
+    #[cfg(debug_assertions)]
+    pub fn test() -> bool {
+        let string = UnicodeString::new();
+        if string.as_str() != "" || string.length() != 0 {
+            println!("creating empty not working");
+            return false;
+        }
+
+        let mut string = UnicodeString::from("test");
+        if string.as_str() != "test" || string.length() != 4 {
+            println!("creating from &str not working");
+            return false;
+        }
+
+        string.insert(4, '!'); // test insertion at end
+        string.insert(0, '_'); // test insertion at start
+        string.insert(5, 'y'); // test insertion in middle
+        string.push('_'); // test pushing
+        if string.as_str() != "_testy!_" || string.length() != 8 {
+            println!("insertion and push not working for '{}'", string.as_str());
+            return false;
+        }
+
+        string.remove(0); // test removal at start
+        string.remove(6); // test removal at end
+        string.remove(4); // test removal in middle
+        if string.as_str() != "test!" || string.length() != 5 {
+            println!("removal not working for '{}'", string.as_str());
+            return false;
+        }
+
+        while string.length() > 0 {
+            string.remove(0); // test removing all characters
+        }
+        if string.as_str() != "" || string.length() != 0 {
+            return false;
+        }
+
+        string.push('å'); // test pushing unicode
+        string.push('ö'); // test pushing unicode
+        string.insert(1, 'ä'); // test inserting unicode
+        if string.as_str() != "åäö" || string.inner_indices != vec![0, 'å'.len_utf8(), "åä".len()] {
+            return false;
+        }
+
+        string.remove(1); // test removing unicode in middle
+        if string.as_str() != "åö" || string.inner_indices != vec![0, 'å'.len_utf8()] {
+            return false;
+        }
+
+        let drained: String = string.drain(0, string.length()).collect(); // test draining all characters
+        if drained.as_str() != "åö" || string.as_str() != "" || string.inner_indices != Vec::new() || string.length() != 0 {
+            return false;
+        } 
+
+        string.push_str("asd"); // test pushing string literal
+        if string.as_str() != "asd" || string.length() != 3 {
+            return false;
+        }
+
+        let drained: String = string.drain(1, string.length()).collect(); // test draining some characters
+        if drained.as_str() != "sd" || string.as_str() != "a" || string.inner_indices != vec![0] || string.length() != 1 {
+            return false;
+        }
+        
+        return true;
+    }
+}
+
+impl Index<RangeFrom<usize>> for UnicodeString {
+    type Output = str;
+
+    fn index(&self, idx: RangeFrom<usize>) -> &Self::Output {
+        let inner_idx = if idx.start == self.length {
+            self.inner_string.len()
+        } else {
+            self.inner_indices[idx.start]
+        };
+        return &self.inner_string[inner_idx..];
+    }
+}
